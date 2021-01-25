0cd6880f239f8394c822b727047c69d7fc8f8735
yesyb <nicaiwho@163.com>
2021-01-25 15:02:10 +0800

    editor: auto adjustify to center when first time add component

--- src/packages/visual-editor-block.tsx
+++ src/packages/visual-editor-block.tsx
@@ -1,21 +1,32 @@
-import { computed, defineComponent, PropType } from "vue";
+import { computed, defineComponent, onMounted, PropType, ref } from "vue";
 import { VisualEditorBlockData, VisualEditorConfig } from "./visual-editor.utils";
 
 export const VisualEditorBlock = defineComponent({
-    props:{
-        block:{type:Object as PropType<VisualEditorBlockData>,required:true},
-        config:{type:Object as PropType<VisualEditorConfig>,required:true}
+    props: {
+        block: { type: Object as PropType<VisualEditorBlockData>, required: true },
+        config: { type: Object as PropType<VisualEditorConfig>, required: true }
     },
-    setup(props){
-        const styles = computed(()=>({
-            top:`${props.block.top}px`,
-            left:`${props.block.left}px`,
-        })) 
-        return ()=>{
+    setup(props) {
+        const el = ref({} as HTMLDivElement);
+        const styles = computed(() => ({
+            top: `${props.block.top}px`,
+            left: `${props.block.left}px`,
+        }))
+        onMounted(() => {
+            /** 添加组件时，自动调整位置上下左右居中 */
+            const block = props.block;
+            if (block.adjustPosition === true) {
+                const {offsetWidth,offsetHeight} = el.value;
+                block.left = block.left - offsetWidth/2;
+                block.top = block.top - offsetHeight/2;
+                block.adjustPosition = false;
+            }
+        });
+        return () => {
             const component = props.config.componentMap[props.block.componentKey];
             const Render = component.render();
             return (
-                <div class="visual-editor-block" style={styles.value}>
+                <div class="visual-editor-block" style={styles.value} ref={el}>
                     {Render}
                 </div>
             )
